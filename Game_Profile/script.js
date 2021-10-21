c7998c1675cdfd8c8d8a9ce2216c910b7cd6a8be
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-10-21 20:42:48 -0300

    Create script.js

---
+++ Game_Profile/script.js
@@ -0,0 +1,11 @@
+var firstName="Caroline";
+var lastName="Silva";
+var username="Username: "+firstName+" "+lastName;
+console.log(username);
+
+var characterName="Carol";
+var character="Character: "+characterName;
+console.log(character);
+
+document.getElementById("username") .innerHTML=username;
+document.getElementById("character") .innerHTML=character;
