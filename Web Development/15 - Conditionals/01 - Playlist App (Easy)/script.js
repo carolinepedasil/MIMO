a1a4077e1622a40b33355124089d7b3efaa9c8ba
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-02-10 19:35:45 -0300

    MIMO

---
+++ Web Development/15 - Conditionals/01 - Playlist App (Easy)/script.js
@@ -0,0 +1,10 @@
+var song = "Don't stop me now";
+var included = true;
+
+console.log("New entry: " + song);
+
+    if (included) {
+        console.log("Already included!");
+    }
+
+console.log("Save and continue");
